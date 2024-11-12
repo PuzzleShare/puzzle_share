fcf82f1cb24322b68e726395deb8cf8cbaa205fe
inwoo <jiw413@naver.com>
2024-11-12 20:35:26 +0900

    fix : flyway 세팅

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
     runtimeOnly("com.mysql:mysql-connector-j")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
---
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE users (
+    user_id BIGINT AUTO_INCREMENT NOT NULL,
+    user_name VARCHAR(50) NOT NULL,
+    email VARCHAR(254) NOT NULL,
+    social_type VARCHAR(50) NOT NULL,
+    user_image VARCHAR(512),
+    created_date DATETIME NOT NULL,
+    modified_date DATETIME NULL,
+    PRIMARY KEY (user_id)
+);
