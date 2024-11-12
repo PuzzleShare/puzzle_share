4d7cf714d95a04478ea299c99c7fb738d95551fb
JINS-DE <rlawlstjd044@naver.com>
2024-11-12 19:29:01 +0900

    fix : Test test

--- backend/src/test/kotlin/com/puzzle/backend/controller/TestControllerTest.kt
+++ backend/src/test/kotlin/com/puzzle/backend/controller/TestControllerTest.kt
@@ -1,6 +1,7 @@
 package com.puzzle.backend.controller
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
@@ -8,16 +9,18 @@ import org.springframework.test.web.servlet.get
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class TestControllerTest(
-    private val mockMvc: MockMvc,
-) {
-    @Test
-    fun `GET success should return Hello World!`() {
-        mockMvc
-            .get("/api/v1/test/success")
-            .andExpect {
-                status { isOk() }
-                content { string("Hello World!") }
-            }
+class TestControllerTest
+    @Autowired
+    constructor(
+        val mockMvc: MockMvc,
+    ) {
+        @Test
+        fun `GET success should return Hello World!`() {
+            mockMvc
+                .get("/api/v1/test/success")
+                .andExpect {
+                    status { isOk() }
+                    content { string("Hello World!") }
+                }
+        }
     }
-}
