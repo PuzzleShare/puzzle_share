a21c170e282da8dc0f952b5f439ec527fab971f8
qudcks1940 <qudcks1940@gmail.com>
2024-11-12 19:59:42 +0900

    feat : 자동 배포 기능 구현

--- backend/src/test/kotlin/com/puzzle/backend/TestControllerTest.kt
+++ backend/src/test/kotlin/com/puzzle/backend/TestControllerTest.kt
@@ -1,30 +1,25 @@
 package com.puzzle.backend
 
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.Conversions.string
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class TestControllerTest
+class TestControllerTest(
     @Autowired
-    constructor(
-        val mockMvc: MockMvc,
-    ) {
-        @Test
-        fun `GET success should return Hello World!`() {
-            mockMvc
-                .get("/api/v1/test/success")
-                .andExpect {
-                    status { isOk() }
-                    content { string("Hello World!") }
-                }
-        }
+    private val mockMvc: MockMvc,
+) {
+    @Test
+    fun `GET success should return Hello World!`() {
+        mockMvc
+            .perform(get("/api/v1/test/success"))
+            .andExpect {
+                it.response.contentAsString.equals("Hello World!")
+            }
     }
+}
