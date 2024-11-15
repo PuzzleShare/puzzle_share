5e2f5d701c1ffa2f7f67a4683d647638e2bad180
JINS-DE <rlawlstjd044@naver.com>
2024-11-15 21:14:55 +0900

    fix : ktlint 수정

--- websocket/src/main/kotlin/com/puzzle/websocket/puzzle/repository/PuzzleRoomRepository.kt
+++ websocket/src/main/kotlin/com/puzzle/websocket/puzzle/repository/PuzzleRoomRepository.kt
@@ -1,8 +1,8 @@
 package com.puzzle.websocket.puzzle.repository
 
 import com.puzzle.websocket.puzzle.domain.PuzzleRoom
-import org.springframework.stereotype.Repository
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
 @Repository
 interface PuzzleRoomRepository : CrudRepository<PuzzleRoom, String>
