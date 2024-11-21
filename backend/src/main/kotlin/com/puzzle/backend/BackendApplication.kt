b1ac49fb09268b61d025e75e49207a1200e25ed7
jifrozen0110 <answldjs1836@gmail.com>
2024-11-21 09:32:05 +0900

    fix : 서버 시간

--- backend/src/main/kotlin/com/puzzle/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/puzzle/backend/BackendApplication.kt
@@ -1,10 +1,8 @@
 package com.puzzle.backend
 
-import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import java.util.TimeZone
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,10 +10,4 @@ class BackendApplication
 
 fun main(args: Array<String>) {
     runApplication<BackendApplication>(*args)
-
-    @PostConstruct
-    fun started() {
-        // timezone UTC 셋팅
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
-    }
 }
