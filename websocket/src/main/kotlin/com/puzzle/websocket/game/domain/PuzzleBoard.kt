bac0340b6607d02bfb2282811b6e7524c4036d18
inwoo <jiw413@naver.com>
2024-11-22 16:47:23 +0900

    refactor : 문법 수정

--- websocket/src/main/kotlin/com/puzzle/websocket/game/domain/PuzzleBoard.kt
+++ websocket/src/main/kotlin/com/puzzle/websocket/game/domain/PuzzleBoard.kt
@@ -1,46 +1,48 @@
 package com.puzzle.websocket.game.domain
 
-import java.util.*
+import java.util.Random
 import kotlin.collections.HashMap
 import kotlin.collections.HashSet
 
 class PuzzleBoard {
-    var picture: Picture? = null // 퍼즐에 쓰이는 사진
+    private var picture: Picture? = null // 퍼즐에 쓰이는 사진
     var idxToCoordinate: HashMap<Int, IntArray> = HashMap()
 
     // 조각들이 들어있는 2차원 배열
     lateinit var board: Array<Array<Piece>>
-    var pieceSize: Int = 0 // 조각 크기
-    var widthCnt: Int = 0 // 가로 조각 개수
-    var lengthCnt: Int = 0 // 세로 조각 개수
+    private var pieceSize: Int = 0 // 조각 크기
+    private var widthCnt: Int = 0 // 가로 조각 개수
+    private var lengthCnt: Int = 0 // 세로 조각 개수
 
-    var totalEdges: Int = 0
-    var connectedEdges: Int = 0
-
+    private var totalEdges: Int = 0
+    private var connectedEdges: Int = 0
 
     // 조합된 퍼즐 뭉탱이들
     val bundles = mutableListOf<MutableSet<Piece>>()
-    lateinit var isCorrected: Array<BooleanArray> // 조합된 퍼즐인지 확인하는 2차원 배열
-    var correctedCount: Int = 0 // 현재까지 맞춘 개수
+    private lateinit var isCorrected: Array<BooleanArray> // 조합된 퍼즐인지 확인하는 2차원 배열
+    private var correctedCount: Int = 0 // 현재까지 맞춘 개수
     var isCompleted: Boolean = false
         private set
 
     // 랜덤 타입 적용에 쓰일 인덱스 상수
-    val TOP = 0
-    val RIGHT = 1
-    val BOTTOM = 2
-    val LEFT = 3
+    private val top = 0
+    private val right = 1
+    private val button = 2
+    private val left = 3
 
-    var visited: Array<BooleanArray> = arrayOf()
-    val CANVAS_WIDTH = 1000
-    val CANVAS_LENGTH = 750
+    private var visited: Array<BooleanArray> = arrayOf()
+    private val canvasWidth = 1000
+    private val canvasLength = 750
 
     // 방향 이동을 위한 배열
-    val dx = arrayOf(1, -1, 0, 0)
-    val dy = arrayOf(0, 0, -1, 1)
+    private val dx = arrayOf(1, -1, 0, 0)
+    private val dy = arrayOf(0, 0, -1, 1)
 
     // 퍼즐 판 초기화
-    fun init(p: Picture, gameType: String): Array<Array<Piece>> {
+    fun init(
+        p: Picture,
+        gameType: String,
+    ): Array<Array<Piece>> {
         picture = p
         pieceSize = p.pieceSize
         widthCnt = p.widthPieceCnt
@@ -85,10 +87,10 @@ class PuzzleBoard {
                 val type = IntArray(4)
 
                 // 각 변에 대한 타입 결정
-                type[TOP] = if (i == 0) 0 else invertType(board[i - 1][j].type[BOTTOM])
-                type[BOTTOM] = if (i == lengthCnt - 1) 0 else randomType()
-                type[LEFT] = if (j == 0) 0 else invertType(board[i][j - 1].type[RIGHT])
-                type[RIGHT] = if (j == widthCnt - 1) 0 else randomType()
+                type[top] = if (i == 0) 0 else invertType(board[i - 1][j].type[button])
+                type[button] = if (i == lengthCnt - 1) 0 else randomType()
+                type[left] = if (j == 0) 0 else invertType(board[i][j - 1].type[right])
+                type[right] = if (j == widthCnt - 1) 0 else randomType()
 
                 now.type = type
 
@@ -102,8 +104,8 @@ class PuzzleBoard {
                 val (randI, randJ) = idxToCoordinate[idx]!!
                 val randomPiece = board[randI][randJ]
                 val x =
-                    (CANVAS_WIDTH / 2 - pieceSize / 2 + pieceSize * ((j * 2) + (i % 2)) - picture!!.imgWidth + 50).toDouble()
-                val y = (CANVAS_LENGTH / 2 - pieceSize / 2 + pieceSize * i - picture!!.imgHeight / 2).toDouble()
+                    (canvasWidth / 2 - pieceSize / 2 + pieceSize * ((j * 2) + (i % 2)) - picture!!.imgWidth + 50).toDouble()
+                val y = (canvasLength / 2 - pieceSize / 2 + pieceSize * i - picture!!.imgHeight / 2).toDouble()
 
                 randomPiece.position_x = x
                 randomPiece.position_y = y
@@ -113,25 +115,27 @@ class PuzzleBoard {
         // 타입 변환 (-1, 1)
         for (i in 0 until lengthCnt) {
             for (j in 0 until widthCnt) {
-                board[i][j].type = board[i][j].type.map {
-                    when (it) {
-                        1 -> -1
-                        2 -> 1
-                        else -> 0
-                    }
-                }.toIntArray()
+                board[i][j].type =
+                    board[i][j]
+                        .type
+                        .map {
+                            when (it) {
+                                1 -> -1
+                                2 -> 1
+                                else -> 0
+                            }
+                        }.toIntArray()
             }
         }
     }
 
     // 타입 반전 함수
-    private fun invertType(type: Int): Int {
-        return when (type) {
+    private fun invertType(type: Int): Int =
+        when (type) {
             1 -> 2
             2 -> 1
             else -> randomType()
         }
-    }
 
     // 랜덤 타입 생성
     private fun randomType(): Int {
@@ -152,7 +156,6 @@ class PuzzleBoard {
         // 병합 상태를 재구성
         searchForGroupDisbandment()
 
-
         updatePieceCount()
 
         // 퍼즐 완성 여부 체크
@@ -162,7 +165,6 @@ class PuzzleBoard {
         }
     }
 
-
     // 맞춘 조각 수 업데이트
     private fun updatePieceCount() {
 //        correctedCount = bundles.sumBy { it.size }
@@ -170,7 +172,7 @@ class PuzzleBoard {
     }
 
     // 결합된 조각 삭제
-    fun deletePiece(targetIdx: Int): DoubleArray? {
+    private fun deletePiece(targetIdx: Int): DoubleArray? {
         val (i, j) = idxToCoordinate[targetIdx]!!
         if (!isCorrected[i][j]) return null
 
@@ -186,7 +188,7 @@ class PuzzleBoard {
     }
 
     // 뭉치 분해 탐색
-    fun searchForGroupDisbandment() {
+    private fun searchForGroupDisbandment() {
         visited = Array(lengthCnt) { BooleanArray(widthCnt) }
         bundles.clear()
 
@@ -206,7 +208,11 @@ class PuzzleBoard {
     }
 
     // DFS 탐색
-    private fun dfsForSearch(r: Int, c: Int, set: MutableSet<Piece>) {
+    private fun dfsForSearch(
+        r: Int,
+        c: Int,
+        set: MutableSet<Piece>,
+    ) {
         visited[r][c] = true
         set.add(board[r][c])
 
@@ -224,8 +230,8 @@ class PuzzleBoard {
     // 조각 랜덤 배치
     private fun randomArrange(pieceIdx: Int): DoubleArray {
         val (i, j) = idxToCoordinate[pieceIdx]!!
-        val x = Random().nextInt(CANVAS_WIDTH).toDouble()
-        val y = Random().nextInt(CANVAS_LENGTH).toDouble()
+        val x = Random().nextInt(canvasWidth).toDouble()
+        val y = Random().nextInt(canvasLength).toDouble()
         board[i][j].position_x = x
         board[i][j].position_y = y
         return doubleArrayOf(x, y)
@@ -255,9 +261,10 @@ class PuzzleBoard {
     }
 
     // 최대공약수 계산 (사용되지 않음)
-    fun gcd(a: Int, b: Int): Int {
-        return if (b == 0) a else gcd(b, a % b)
-    }
+//    fun gcd(
+//        a: Int,
+//        b: Int,
+//    ): Int = if (b == 0) a else gcd(b, a % b)
 
     // PuzzleBoard 클래스 내부
     fun calculateMixedProgress(): Double {
@@ -283,5 +290,4 @@ class PuzzleBoard {
         val progress = alpha * pieceProgress + (1 - alpha) * edgeProgress
         return if (progress > 100) 100.0 else progress // 진행률을 최대 100으로 제한
     }
-
 }
