95081c0da1f9fcfe72d272a1fba365991654e86e
JINS-DE <rlawlstjd044@naver.com>
2024-11-22 14:13:23 +0900

    feature : 게임 이미지 custom 적용

--- websocket/src/main/kotlin/com/puzzle/websocket/game/domain/Game.kt
+++ websocket/src/main/kotlin/com/puzzle/websocket/game/domain/Game.kt
@@ -142,28 +142,35 @@ data class Game(
             val name = room.roomName
             val roomSize = room.maxPlayers
             val gameType = "BATTLE"
+            val puzzleImage = room.puzzleImage
             val uuid = UUID.randomUUID().toString()
 
-            val game =
-                Game(
-                    gameId = uuid,
-                    gameName = name,
-                    roomSize = roomSize,
-                    gameType = gameType,
-                    sessionToUser = mutableMapOf(),
-                )
+            // PuzzleRoom에서 받은 puzzleImage 값을 Picture에 전달
+            val picture = Picture.create(
+                width = 1000, // 적절한 width 값을 설정하세요.
+                length = 551, // 적절한 length 값을 설정하세요.
+                pieceSize = 40, // 적절한 퍼즐 조각 크기를 설정하세요.
+                imageName = puzzleImage,
+                encodedString = puzzleImage
+            )
+
+            val game = Game(
+                gameId = uuid,
+                gameName = name,
+                roomSize = roomSize,
+                gameType = gameType,
+                sessionToUser = mutableMapOf(),
+                picture = picture // 생성한 Picture 객체를 설정
+            )
 
             if (gameType == "BATTLE") {
                 game.redTeam = room.redPlayers
-
                 game.blueTeam = room.bluePlayers
-                game.picture = Picture.create()
                 game.startTime = Date()
                 println("$name 배틀 방 생성 / id = $uuid")
             } else if (gameType == "COOPERATION") {
                 game.redTeam = room.redPlayers
                 game.blueTeam = room.bluePlayers
-                game.picture = Picture.create()
                 game.startTime = Date()
                 println("$name 협동 방 생성 / id = $uuid")
             }
