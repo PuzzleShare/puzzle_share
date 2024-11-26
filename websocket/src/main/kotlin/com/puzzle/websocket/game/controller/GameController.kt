3584a6739a095c12f7a7d812654aa9333dee67f8
qudcks1940 <qudcks1940@gmail.com>
2024-11-26 22:50:05 +0900

    feat: 게임 전적 websocket 코드 수정

--- websocket/src/main/kotlin/com/puzzle/websocket/game/controller/GameController.kt
+++ websocket/src/main/kotlin/com/puzzle/websocket/game/controller/GameController.kt
@@ -12,7 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Controller
 import org.springframework.web.socket.messaging.SessionConnectEvent
-import java.util.Queue
+import java.util.*
 import java.util.concurrent.ConcurrentLinkedQueue
 
 @Controller
@@ -21,7 +21,7 @@ class GameController(
     private val gameService: GameService,
     private val sendingOperations: SimpMessageSendingOperations,
 ) {
-    private val battleTimer = 180
+    private val battleTimer = 10
     private var sessionId: String? = null
     private val waitingList: Queue<User> = ConcurrentLinkedQueue()
 
@@ -105,7 +105,7 @@ class GameController(
     fun sendServerTime() {
         val allRooms = gameService.findAllCooperationRoom() + gameService.findAllBattleRoom()
         for (game in allRooms.reversed()) {
-            if (game.isStarted) {
+            if (game.isStarted && !game.isFinished) {
                 var time = game.getTime()
                 if (game.gameType == "BATTLE") {
                     time = battleTimer - time
@@ -114,14 +114,37 @@ class GameController(
                     val timer = mapOf("time" to time)
                     sendingOperations.convertAndSend("/topic/game/room/${game.gameId}", timer)
                 } else {
-                    val res = ResponseMessage()
-                    res.isFinished = true
-
-                    Thread.sleep(20)
+                    game.finishTime = Date()
+                    val res = ResponseMessage(game = game).apply {
+                        redProgressPercent = game.redPuzzle?.calculateMixedProgress() ?: 0.0
+                        blueProgressPercent = if (game.gameType == "BATTLE") {
+                            game.bluePuzzle?.calculateMixedProgress() ?: 0.0
+                        } else {
+                            -1.0
+                        }
+                        redBundles = game.redPuzzle
+                            ?.bundles
+                            ?.values
+                            ?.map { it.toSet() } ?: emptyList()
+                        blueBundles = if (game.gameType.equals("BATTLE", ignoreCase = true)) {
+                            game.bluePuzzle
+                                ?.bundles
+                                ?.values
+                                ?.map { it.toSet() } ?: emptyList()
+                        } else {
+                            emptyList()
+                        }
+                    }
+                    game.isFinished = true
+                    res.isFinished = game.isFinished
                     sendingOperations.convertAndSend("/topic/game/room/${game.gameId}", res)
+
+                    game.isStarted = false
+                    Thread.sleep(20)
                     gameService.deleteGame(game.gameId)
                 }
             }
         }
     }
+
 }
