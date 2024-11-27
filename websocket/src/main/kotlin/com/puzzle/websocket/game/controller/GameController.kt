5def6457c0adfd02000d3f0daf8a2875040730a6
minjae <minjagot@gmail.com>
2024-11-27 12:57:05 +0900

    feat : team pointer

--- websocket/src/main/kotlin/com/puzzle/websocket/game/controller/GameController.kt
+++ websocket/src/main/kotlin/com/puzzle/websocket/game/controller/GameController.kt
@@ -4,6 +4,7 @@ import com.puzzle.websocket.game.domain.ResponseMessage
 import com.puzzle.websocket.game.domain.SharePuzzle
 import com.puzzle.websocket.game.domain.User
 import com.puzzle.websocket.game.dto.response.InventoryResponse
+import com.puzzle.websocket.game.dto.response.PointerMoveDTO
 import com.puzzle.websocket.game.service.GameService
 import org.springframework.context.event.EventListener
 import org.springframework.messaging.handler.annotation.DestinationVariable
@@ -38,30 +39,32 @@ class GameController(
         @DestinationVariable roomId: String,
     ) {
         val game = gameService.findById(roomId)!!
-        sendingOperations.convertAndSend(
-            "/topic/game/room/$roomId/init",
-            ResponseMessage(game = game).apply {
-                // 혼합 방식 진행률 계산 반영
-                redProgressPercent = game.redPuzzle?.calculateMixedProgress() ?: 0.0
-                blueProgressPercent =
-                    if (game.gameType == "BATTLE") {
-                        game.bluePuzzle?.calculateMixedProgress() ?: 0.0
-                    } else {
-                        0.0
-                    }
-                isFinished = game.isFinished
-                redBundles = game.redPuzzle
+        val res = ResponseMessage(game = game).apply {
+            // 혼합 방식 진행률 계산 반영
+            redProgressPercent = game.redPuzzle?.calculateMixedProgress() ?: 0.0
+            blueProgressPercent =
+                if (game.gameType == "BATTLE") {
+                    game.bluePuzzle?.calculateMixedProgress() ?: 0.0
+                } else {
+                    0.0
+                }
+            isFinished = game.isFinished
+            redBundles = game.redPuzzle
+                ?.bundles
+                ?.values
+                ?.map { it.toSet() } ?: emptyList()
+            if (game.gameType.equals("BATTLE", ignoreCase = true)) {
+                blueBundles = game.bluePuzzle
                     ?.bundles
                     ?.values
                     ?.map { it.toSet() } ?: emptyList()
-                if (game.gameType.equals("BATTLE", ignoreCase = true)) {
-                    blueBundles = game.bluePuzzle
-                        ?.bundles
-                        ?.values
-                        ?.map { it.toSet() } ?: emptyList()
-                }
-            },
-        )
+            }
+        }
+        sendingOperations.convertAndSend("/topic/game/room/$roomId/init", res)
+
+        val pointerMoveDTOS = game.redTeam.map { PointerMoveDTO.of(it, "red", "red") } +
+                game.blueTeam.map { PointerMoveDTO.of(it, "blue", "blue") }
+        sendingOperations.convertAndSend("/topic/game/${game.gameId}/pointer/init", pointerMoveDTOS)
     }
 
     @MessageMapping("/game/puzzle")
@@ -117,29 +120,39 @@ class GameController(
         sendingOperations.convertAndSend("/topic/game/room/${sharePuzzle.roomId}", res)
     }
 
-    //  서버 타이머 제공
-    @Scheduled(fixedRate = 1000)
+    @MessageMapping("/game/{gameId}/mouse")
     @Throws(Exception::class)
-    fun sendServerTime() {
-        val allRooms = gameService.findAllCooperationRoom() + gameService.findAllBattleRoom()
-        for (game in allRooms.reversed()) {
-            if (game.isStarted) {
-                var time = game.getTime()
-                if (game.gameType == "BATTLE") {
-                    time = battleTimer - time
-                }
-                if (time >= 0) {
-                    val timer = mapOf("time" to time)
-                    sendingOperations.convertAndSend("/topic/game/room/${game.gameId}", timer)
-                } else {
-                    val res = ResponseMessage()
-                    res.isFinished = true
-
-                    Thread.sleep(20)
-                    sendingOperations.convertAndSend("/topic/game/room/${game.gameId}", res)
-                    gameService.deleteGame(game.gameId)
-                }
-            }
-        }
+    fun pointerMove(
+        @DestinationVariable
+        gameId: String,
+        pointerMoveDTO: PointerMoveDTO
+    ){
+        sendingOperations.convertAndSend("/topic/game/$gameId/mouse", pointerMoveDTO)
     }
+
+    //  서버 타이머 제공
+//    @Scheduled(fixedRate = 1000)
+//    @Throws(Exception::class)
+//    fun sendServerTime() {
+//        val allRooms = gameService.findAllCooperationRoom() + gameService.findAllBattleRoom()
+//        for (game in allRooms.reversed()) {
+//            if (game.isStarted) {
+//                var time = game.getTime()
+//                if (game.gameType == "BATTLE") {
+//                    time = battleTimer - time
+//                }
+//                if (time >= 0) {
+//                    val timer = mapOf("time" to time)
+//                    sendingOperations.convertAndSend("/topic/game/room/${game.gameId}", timer)
+//                } else {
+//                    val res = ResponseMessage()
+//                    res.isFinished = true
+//
+//                    Thread.sleep(20)
+//                    sendingOperations.convertAndSend("/topic/game/room/${game.gameId}", res)
+//                    gameService.deleteGame(game.gameId)
+//                }
+//            }
+//        }
+//    }
 }
